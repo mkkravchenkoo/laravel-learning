81dc43c06e420b0d35c3c7ad2cb6d453f889c0cb
mkkravchenkoo <mkkravchenkoo@gmail.com>
2023-09-30 13:17:05 +0300

    routes - separate in files

--- routes/main.php
+++ routes/main.php
@@ -1,7 +1,8 @@
 <?php
 
-use App\Http\Controllers\PostController;
+use App\Http\Controllers\LoginController;
 use App\Http\Controllers\Posts\CommentController;
+use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\TestController;
 use Illuminate\Support\Facades\Route;
 
@@ -17,16 +18,12 @@ use Illuminate\Support\Facades\Route;
 */
 Route::get('/test', TestController::class);
 
-Route::prefix('user')->group(function (){
-    Route::get('/posts', [PostController::class, 'index'])->name('user.posts.index');
-    Route::get('/posts/create', [PostController::class, 'create'])->name('user.posts.create');
-    Route::post('/posts', [PostController::class, 'store'])->name('user.posts.store');
-    Route::get('/posts/{post}', [PostController::class, 'show'])->name('user.posts.show');
-    Route::get('/posts/{post}/edit', [PostController::class, 'edit'])->name('user.posts.edit');
-    Route::put('/posts/{post}', [PostController::class, 'update'])->name('user.posts.update');
-    Route::delete('/posts/{post}', [PostController::class, 'delete'])->name('user.posts.delete');
-    Route::put('/posts/{post}/like', [PostController::class, 'like'])->name('user.posts.like');
-});
+Route::get('/register', [RegisterController::class, 'index'])->name('register');
+Route::post('/register', [RegisterController::class, 'store'])->name('register.store');
+
+Route::post('/login', [LoginController::class, 'login'])->name('login');
+Route::get('/login', [LoginController::class, 'store'])->name('login.store');
+
 
 Route::resource('posts/{post}/comments', CommentController::class);
 
