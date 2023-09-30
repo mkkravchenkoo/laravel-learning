9a2fdd9f76e02e96c06da57c28d310fa0d63c634
mkkravchenkoo <mkkravchenkoo@gmail.com>
2023-09-30 13:05:08 +0300

    prefix for route and group

--- routes/web.php
+++ routes/web.php
@@ -17,14 +17,16 @@ use Illuminate\Support\Facades\Route;
 */
 Route::get('/test', TestController::class);
 
-Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
-Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');
-Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
-Route::get('/posts/{post}', [PostController::class, 'show'])->name('posts.show');
-Route::get('/posts/{post}/edit', [PostController::class, 'edit'])->name('posts.edit');
-Route::put('/posts/{post}', [PostController::class, 'update'])->name('posts.update');
-Route::delete('/posts/{post}', [PostController::class, 'delete'])->name('posts.delete');
-Route::put('/posts/{post}/like', [PostController::class, 'like'])->name('posts.like');
+Route::prefix('user')->group(function (){
+    Route::get('/posts', [PostController::class, 'index'])->name('user.posts.index');
+    Route::get('/posts/create', [PostController::class, 'create'])->name('user.posts.create');
+    Route::post('/posts', [PostController::class, 'store'])->name('user.posts.store');
+    Route::get('/posts/{post}', [PostController::class, 'show'])->name('user.posts.show');
+    Route::get('/posts/{post}/edit', [PostController::class, 'edit'])->name('user.posts.edit');
+    Route::put('/posts/{post}', [PostController::class, 'update'])->name('user.posts.update');
+    Route::delete('/posts/{post}', [PostController::class, 'delete'])->name('user.posts.delete');
+    Route::put('/posts/{post}/like', [PostController::class, 'like'])->name('user.posts.like');
+});
 
 Route::resource('posts/{post}/comments', CommentController::class);
 
