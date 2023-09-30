9590c80a3f51acf5d875dc3da9a1b6f383008d27
mkkravchenkoo <mkkravchenkoo@gmail.com>
2023-09-30 12:59:06 +0300

    controllers and route - some structure

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\Posts\CommentController;
+use App\Http\Controllers\TestController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,7 +15,27 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+Route::get('/test', TestController::class);
+
+Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
+Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');
+Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
+Route::get('/posts/{post}', [PostController::class, 'show'])->name('posts.show');
+Route::get('/posts/{post}/edit', [PostController::class, 'edit'])->name('posts.edit');
+Route::put('/posts/{post}', [PostController::class, 'update'])->name('posts.update');
+Route::delete('/posts/{post}', [PostController::class, 'delete'])->name('posts.delete');
+Route::put('/posts/{post}/like', [PostController::class, 'like'])->name('posts.like');
+
+Route::resource('posts/{post}/comments', CommentController::class);
+
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::view('/', 'welcome'); // the same
+
+Route::redirect('/home', '/');
+//Route::fallback(function (){
+//    return 'our fallback';
+//});
+
